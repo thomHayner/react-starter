85f601a2399ea023cfba26cc263fc42db7e478a0
Thomas Hayner <thomas@Thomass-MacBook-Pro.local>
2020-02-20 11:33:06 -0700

    Complete App Functionality - Stage 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import MovieList from './MovieList.js';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'MEGA ULTIMATE SUPER AWESOME ALL TIME BEST MOVIE LIST!!!!!!!',
+      movieList: ['Die Hard', 'Die Hard 2', 'Die Hard, with a Vengence', 'Live Free or Die Hard', 'A Good Day to Die Hard']
+    }
+  }
   render(){
     return(
-    <div>
-      Starter App component - hello there
+    <div className='main'>
+      <h2 className='page-name'>{this.state.name}</h2>
+      <MovieList movies={this.state.movieList} />
     </div>
   )}
 }
